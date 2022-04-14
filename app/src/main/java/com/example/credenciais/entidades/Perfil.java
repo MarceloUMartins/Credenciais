5957461dcd9ab24fcfdcfa3ff0fc4006f660a0b8
Júlio Mesquita <julio.mesquita@geosales.com.br>
2022-04-14 01:54:08 -0300

    Criando objeto perfil

---
+++ app/src/main/java/com/example/credenciais/entidades/Perfil.java
@@ -0,0 +1,67 @@
+package com.example.credenciais.entidades;
+
+public class Perfil {
+    private String nome;
+    private String sexo;
+    private String email;
+    private String senha;
+    private String telefone;
+    private String disciplina;
+    private String turma;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+}
