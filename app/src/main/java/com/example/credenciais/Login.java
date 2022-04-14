4d30fa4ac73e439ee74ec42ff0b5f48371f17c27
Júlio Mesquita <julio.mesquita@geosales.com.br>
2022-04-14 01:50:30 -0300

    Ajustando comportamento dos Botões na tela de Login

--- app/src/main/java/com/example/credenciais/Login.java
+++ app/src/main/java/com/example/credenciais/Login.java
@@ -1,11 +1,16 @@
 package com.example.credenciais;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
 
@@ -13,10 +18,25 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = findViewById(R.id.toolbarLogin);
+        setSupportActionBar(toolbar);
+
+        Button buttonregistrar = findViewById(R.id.buttonRegistrar);
+        buttonregistrar.setOnClickListener(v -> registrar());
+
+        Button buttonLogin = findViewById(R.id.button);
+        buttonLogin.setOnClickListener(v -> logIn());
+
     }
 
-    public void registrar(View view) {
+    public void registrar() {
         Intent intent = new Intent(this, Cadastro.class);
+        startActivity(intent);
+    }
+
+    public void logIn() {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
