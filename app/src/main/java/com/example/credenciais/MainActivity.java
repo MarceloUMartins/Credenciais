737f9900b679fe867bc1fcd64a9593ec7f46121a
Júlio Mesquita <julio.mesquita@geosales.com.br>
2022-04-14 21:48:31 -0300

    exibindo nome do Perfil após o Login

--- app/src/main/java/com/example/credenciais/MainActivity.java
+++ app/src/main/java/com/example/credenciais/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.credenciais;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.credenciais.entidades.Perfil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Perfil perfil = (Perfil) getIntent().getSerializableExtra("perfil");
+        TextView nomePerfil = findViewById(R.id.nomePerfil);
+        nomePerfil.setText("Bem-vindo, "+ perfil.getNome());
     }
 }
