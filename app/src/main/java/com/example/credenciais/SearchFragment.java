d3b3f61cc39a01ea94b2922949439d138891ae80
Júlio Mesquita <julio.mesquita@geosales.com.br>
2022-05-17 23:54:03 -0300

    inserindo lista de perfil que consome do banco de dados

--- app/src/main/java/com/example/credenciais/SearchFragment.java
+++ app/src/main/java/com/example/credenciais/SearchFragment.java
@@ -4,15 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.credenciais.Mapper.PerfilMapper;
+import com.example.credenciais.entidades.Perfil;
+
+import java.util.List;
 
 public class SearchFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        PerfilMapper perfilMapper = new PerfilMapper(this.getContext());
+
+        List<Perfil> data = perfilMapper.resgatePerfis();
+
+        ArrayAdapter<Perfil> adapter = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_list_item_1, data);
+
+        ListView listView = view.findViewById(R.id.listaPerfis);
+
+        listView.setAdapter(adapter);
+        return view;
     }
 }
