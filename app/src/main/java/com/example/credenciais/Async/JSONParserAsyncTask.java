8d1fbb1d84e92605045368afaa056d0ccbe5a09c
Júlio Mesquita <julio.mesquita@geosales.com.br>
2022-06-15 16:34:48 -0300

    exibindo temperatura usando o AsyncTask

---
+++ app/src/main/java/com/example/credenciais/Async/JSONParserAsyncTask.java
@@ -0,0 +1,83 @@
+package com.example.credenciais.Async;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class JSONParserAsyncTask extends AsyncTask<Void, Void, String> {
+        private final TextView mtextView;
+        private final String mUrl;
+
+        public JSONParserAsyncTask(TextView textView, String url) {
+            mtextView = textView;
+            mUrl = url;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            return getJSONTemp(mUrl);
+        }
+
+        @Override
+        protected void onPostExecute(String strings) {
+            super.onPostExecute(strings);
+            mtextView.setText(strings);
+        }
+
+        private String getJSONTemp(String url) {
+            HttpURLConnection c = null;
+            try {
+                URL u = new URL(url);
+                c = (HttpURLConnection) u.openConnection();
+                c.connect();
+                int status = c.getResponseCode();
+                switch (status) {
+                    case 200:
+                    case 201:
+                        BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            sb.append(line+"\n");
+                        }
+                        br.close();
+
+                        return jsonParseGetTemp(sb.toString());
+                }
+
+            } catch (Exception ex) {
+                return ex.toString();
+            } finally {
+                if (c != null) {
+                    try {
+                        c.disconnect();
+                    } catch (Exception ex) {
+                        //disconnect error
+                    }
+                }
+            }
+            return "Falha";
+        }
+
+        private String jsonParseGetTemp(String result) throws JSONException {
+            JSONObject jObject = new JSONObject(result);
+            JSONObject jObject2 = jObject.getJSONObject("current_weather");
+            String temperatura = jObject2.getString("temperature");
+            return temperatura + " ºC";
+        }
+
+
+}
--- app/src/main/java/com/example/credenciais/HomeFragment.java
+++ app/src/main/java/com/example/credenciais/HomeFragment.java
@@ -9,14 +9,18 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.credenciais.Async.JSONParserAsyncTask;
+
 public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        TextView nomePerfil = view.findViewById(R.id.nomePerfilHome);
-        nomePerfil.setText("Bem-vindo!");
+        TextView temperaturaHome = view.findViewById(R.id.temperatura);
+
+        JSONParserAsyncTask jsonParserAsyncTask = new JSONParserAsyncTask(temperaturaHome, "https://api.open-meteo.com/v1/forecast?latitude=-3.758008382787574&longitude=-38.517125982773706&current_weather=true");
+        jsonParserAsyncTask.execute();
         return view;
     }
 }
